c672ef09ee3355e42528e946af69e532dd3e4477
kam3r0nz <kam.hungerford@gmail.com>
2020-11-07 22:59:08 -0600

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,12 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def is_active?
+    if self.active == false
+      "This student is currently inactive."
+    elsif self.active == true
+      "This student is currently active."
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+student1 = Student.create(first_name: "Kameron", last_name: "Hungerford")
+
+student2 = Student.create(first_name: "Grace", last_name: "Whaley")
+
+student3 = Student.create(first_name: "Blake", last_name: "Peters")
